efe9f8a1a232f598b79a80b9ac9798a51269e252
Karl Muldia <karlkustav26@gmail.com>
2022-12-06 18:47:30 +0200

    read_product_info()

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
+import re
+from time import sleep
+
 import bs4 as bs
 import requests
 from selenium import webdriver
-from time import sleep
 from unidecode import unidecode
+
 # - - - - -
 
-def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
+def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi and add to items_to_check.txt
     link_prisma = "https://www.prismamarket.ee/products/search/" + item
     link_rimi = "https://www.rimi.ee/epood/ee/otsing?query=" + item
     link_selver = "https://www.selver.ee/search?q=" + item
@@ -23,6 +26,7 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
                     for a in d.find_all('a', class_="card__url js-gtm-eec-product-click"):
                         rimi_shop_links.append('https://www.rimi.ee' + a['href'])
 
+        # PRISMA: Find links to all matching items
         elif link == link_prisma:
             prisma_shop_links = []
             browser = webdriver.Chrome(executable_path='chromedriver.exe')
@@ -35,6 +39,7 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
                         if item.lower() in unidecode(img['alt'].lower()):
                             prisma_shop_links.append('https://www.prismamarket.ee' + a['href'])
 
+        # SELVER: Find links to all matching items
         elif link == link_selver:
             selver_shop_links = []
             browser = webdriver.Chrome(executable_path='chromedriver.exe')
@@ -53,7 +58,7 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
     print('INFO: Added %s items from Prisma, %s items from Rimi and %s items from Selver to the list.' % (len(prisma_shop_links), len(rimi_shop_links), len(selver_shop_links)))
 
 
-def get_all_items():
+def get_all_items(): # Output all items from items_to_check.txt as a list
     output = []
 
     with open('items_to_check.txt', 'r') as f:
@@ -63,10 +68,60 @@ def get_all_items():
                 output.append(eval(i))
     return output
 
-def read_product_page_info(link):
-    if 'prisma' in link:
-        pass
+def read_product_info(link): # Read product name, price/l and alc. volume from Prisma, Rimi or Selver
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    name = ''
+    price = ''
+    volume = ''
+
     if 'rimi' in link:
-        pass
+        soup = bs.BeautifulSoup(requests.get(link, headers=headers).text, features='html.parser')
+        
+        for i in soup.find_all('h3', class_='name'):
+            name = i.text.strip()
+        
+        for i in soup.find_all('p', class_='price-per'):
+            i = i.text.strip().replace(' ', '').split('\n')
+            price = float(i[0].replace('€/l', '').replace(',', '.').replace('€', ''))
+
+        name_info = name.split(' ')
+        for i in name_info:
+            if '%' in i:
+                volume = float(i.replace('%', '').replace('vol', '').replace(',', '.'))
+
+    if 'prisma' in link:
+        soup = bs.BeautifulSoup(requests.get(link, headers=headers).text, features='html.parser')
+        
+        for i in soup.find_all('h1', id='product-name'):
+            name = i.text.strip()
+        
+        for i in soup.find_all('div', class_='details text-right'):
+            i = i.text.strip().replace(' ', '').split('\n')
+            price = float(i[2].replace('€/l', '').replace(',', '.'))
+
+        for i in soup.find_all('div', class_='aisle'):
+            i = i.text.strip().replace(' ', '').replace('\n', '').split(':')
+            volume = float(i[len(i)-1].replace('%', ''))
+
     if 'selver' in link:
-        pass
+        browser = webdriver.Chrome(executable_path='chromedriver.exe')
+        browser.get(link)
+        soup = bs.BeautifulSoup(browser.page_source, features='html.parser')
+
+        for i in soup.find_all('h1', class_='ProductName'):
+            name = i.text.strip()
+
+        for i in soup.find('span', class_='ProductPrice__unit-price'):
+            price = float(i.strip().replace('€/l', '').replace(',', '.').replace(' ', ''))
+
+        for i in soup.find('td', class_='ProductAttributes__value p0 pr30'):
+            i = i.strip().split(' ')
+            if '%' in i[1]:
+                volume = float(i[1].replace('%', ''))
+
+    return{'name': name, 'price': price, 'volume': volume}
+
+def calculate_parmukoefitsient(item): # Calculates parmukoefitsient. Expects {'name': name, 'price': price, 'volume': volume} as parameter.
+    return item['volume'] / item['price']
+
+print(read_product_info('https://www.selver.ee/likoor-jagermeister-100-cl'))
---
+++ settings.py
@@ -0,0 +1,6 @@
+# Settings for the program.
+
+# Ignore 80% volume alcoholic drinks
+ignore_over_80 = False
+
+# 
