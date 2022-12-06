d639ad462389a61bc294e7e501cae94458b25714
Karl Muldia <karlkustav26@gmail.com>
2022-12-06 15:22:59 +0200

    get_all_items() and fix previous

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import bs4 as bs
 import requests
 from selenium import webdriver
 from time import sleep
+from unidecode import unidecode
 # - - - - -
 
 def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
@@ -9,16 +10,16 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
     link_rimi = "https://www.rimi.ee/epood/ee/otsing?query=" + item
     link_selver = "https://www.selver.ee/search?q=" + item
     
-    for link in [link_prisma, link_rimi, link_selver]:
+    for link in [link_rimi, link_prisma, link_selver]:
         # Create soup object from link using requests and bs4
 
         # RIMI: Find links to all matching items
         if link == link_rimi:
+            rimi_shop_links = []
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
             soup = bs.BeautifulSoup(requests.get(link, headers=headers).text, features='html.parser')
-            rimi_shop_links = []
             for d in soup.find_all('div', class_="js-product-container card -horizontal-for-mobile"):
-                if item in d['data-gtms-click-name']:
+                if item.lower() in unidecode(d['data-gtms-click-name'].lower()):
                     for a in d.find_all('a', class_="card__url js-gtm-eec-product-click"):
                         rimi_shop_links.append('https://www.rimi.ee' + a['href'])
 
@@ -31,7 +32,7 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
             for a in soup.find_all('a', class_="js-link-item"):
                 for div in a.find_all('div'):
                     for img in div.find_all('img'):
-                        if item in img['alt']:
+                        if item.lower() in unidecode(img['alt'].lower()):
                             prisma_shop_links.append('https://www.prismamarket.ee' + a['href'])
 
         elif link == link_selver:
@@ -49,3 +50,23 @@ def add_item_to_list(item): # Search for item on Prisma, Selver, Rimi
     with open('items_to_check.txt', 'a') as f:
         f.write(str(add_to_checklist) + '\n')
 
+    print('INFO: Added %s items from Prisma, %s items from Rimi and %s items from Selver to the list.' % (len(prisma_shop_links), len(rimi_shop_links), len(selver_shop_links)))
+
+
+def get_all_items():
+    output = []
+
+    with open('items_to_check.txt', 'r') as f:
+        for i in f.readlines():
+            i = i.strip()
+            if i != '':
+                output.append(eval(i))
+    return output
+
+def read_product_page_info(link):
+    if 'prisma' in link:
+        pass
+    if 'rimi' in link:
+        pass
+    if 'selver' in link:
+        pass
